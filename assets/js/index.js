124463b1db3e048ff8670431dce0259bc059f55c
mason ashlock <masonashlock@gmail.com>
2019-05-07 20:05:20 -0500

    updated portfolio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
-$(function () {
-    $('[data-toggle="popover"]').popover()
-});
+$(document).ready(function () {
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+    //please don't destroy my database :-(
+    var config = {
+        apiKey: "AIzaSyBOgwJDxn7HXarmPFhIBbDsumFaPvS6Cm0",
+        authDomain: "profile-resonses.firebaseapp.com",
+        databaseURL: "https://profile-resonses.firebaseio.com",
+        projectId: "profile-resonses",
+        storageBucket: "profile-resonses.appspot.com",
+        messagingSenderId: "237377136527"
+    };
+    firebase.initializeApp(config);
+
+    $('#submit').click(function (e) {
+        e.preventDefault();
+        console.log(e)
+
+    })
+})
